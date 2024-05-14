b22fd90a972248894ab62fd988119a7232041713
camilomdeoca <camilomdeoca@gmail.com>
2024-05-14 17:01:14 -0300

    fix subtract wasnt static function

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ export class Vector2 {
     return new Vector2(v1.x + v2.x, v1.y + v2.y);
   }
 
-  public subtract(v1: Vector2, v2: Vector2): Vector2
+  public static subtract(v1: Vector2, v2: Vector2): Vector2
   {
     return new Vector2(v1.x - v2.x, v1.y - v2.y);
   }
